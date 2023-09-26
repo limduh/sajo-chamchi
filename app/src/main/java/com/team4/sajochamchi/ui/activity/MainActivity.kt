a544f9ccfb82c4e40144f9ff082bb1e830fff98d
PotatoMeme <kshk0897@gmail.com>
2023-09-26 09:41:15 +0900

    [FEAT] : ⚙️ HomeFragment 생성 및 연결

--- app/src/main/java/com/team4/sajochamchi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/team4/sajochamchi/ui/activity/MainActivity.kt
@@ -2,10 +2,17 @@ package com.team4.sajochamchi.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team4.sajochamchi.R
 import com.team4.sajochamchi.databinding.ActivityMainBinding
+import com.team4.sajochamchi.ui.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment: HomeFragment = HomeFragment.newInstance()
+    //private val searchFragment : HomeFragment = HomeFragment.newInstance()
+    //private val myPageFragment : HomeFragment = HomeFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +24,17 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() = with(binding) {
         //nav view
         bottomNavigationView.itemIconTintList = null
+
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.frame_layout, homeFragment).commit()
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_home -> {
+                    fragmentManager.beginTransaction().replace(R.id.frame_layout, homeFragment)
+                        .commit()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
---
+++ app/src/main/java/com/team4/sajochamchi/ui/fragment/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.team4.sajochamchi.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.team4.sajochamchi.R
+import com.team4.sajochamchi.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding : FragmentHomeBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() = with(binding){
+
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+}
