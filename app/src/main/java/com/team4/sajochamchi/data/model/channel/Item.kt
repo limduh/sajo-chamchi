fec88cd98ffbd7d8deb92fdddcc0ad541653737b
PotatoMeme <kshk0897@gmail.com>
2023-09-27 12:39:02 +0900

    [FEAT] : ⚙️ 채널 보여주기 변환용 model 추가

--- app/src/main/java/com/team4/sajochamchi/data/model/channel/Item.kt
+++ app/src/main/java/com/team4/sajochamchi/data/model/channel/Item.kt
@@ -1,9 +1,17 @@
 package com.team4.sajochamchi.data.model.channel
 
+import com.team4.sajochamchi.data.model.SaveChannel
+
 data class Item(
     val etag: String,
     val id: String,
     val kind: String,
-    val snippet: Snippet
+    val snippet: Snippet,
 )
 
+fun Item.toSaveChannel() = SaveChannel(
+    id = id,
+    title = snippet.title,
+    desciptions = snippet.description,
+    thumbnailUrl = snippet.thumbnails.high.url
+)
