3b76cef1c5d1b0a64b59346b7d7fcf344d9417c2
Lim-Juri <imjuly05@gmail.com>
2023-10-02 11:40:49 +0900

    [FEAT] : ⚙️: SplashActivity 애니메이션 효과, 시간 지연 이후 실행 기능 추가

--- app/src/main/java/com/team4/sajochamchi/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/team4/sajochamchi/ui/activity/SplashActivity.kt
@@ -3,25 +3,31 @@ package com.team4.sajochamchi.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import com.team4.sajochamchi.R
 import com.team4.sajochamchi.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var binding : ActivitySplashBinding
+    lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initViews()
-    }
+        val animation = AnimationUtils.loadAnimation(this, R.anim.anim_splash_imageview)
+        binding.splashImageView.startAnimation(animation)
 
-    private fun initViews() = with(binding){
-        fabImageButton.setOnClickListener {
-            val intent = Intent(this@SplashActivity,MainActivity::class.java)
+        // 일정 시간 지연 이후 실행하기 위한 코드
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 일정 시간이 지나면 MainActivity로 이동
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.anim_splash_out_top,R.anim.anim_splash_in_down)
             finish()
-        }
+        }, 0)
     }
 }
