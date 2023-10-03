d3c663ac6da1ba76ea21b9258de225a63d747f6c
PotatoMeme <kshk0897@gmail.com>
2023-10-03 19:04:49 +0900

    [FEAT] : ⚙️ 앤터로 검색 하기 && x버튼을 누를경우 editText의 내용 삭제

--- app/src/main/java/com/team4/sajochamchi/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/team4/sajochamchi/ui/fragment/SearchFragment.kt
@@ -1,12 +1,20 @@
 package com.team4.sajochamchi.ui.fragment
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.team4.sajochamchi.R
 import com.team4.sajochamchi.data.repository.TotalRepositoryImpl
 import com.team4.sajochamchi.databinding.FragmentSearchBinding
 import com.team4.sajochamchi.ui.dialog.ViewDetailDialog
@@ -45,14 +53,74 @@ class SearchFragment : Fragment() {
         var selected = true
         searchHistoryStatusImageView.isSelected = selected
         searchHistoryStatusImageView.setOnClickListener {
-            if (selected){
+            if (selected) {
                 recentlySearchRecyclerView.visibility = View.GONE
-            }else{
+            } else {
                 recentlySearchRecyclerView.visibility = View.VISIBLE
             }
             selected = !selected
             searchHistoryStatusImageView.isSelected = selected
         }
+
+        // 입력 관련
+        searchEditText.apply {
+            val searchDrawble = searchEditText.compoundDrawablesRelative[2]
+            val clearDrawable = searchEditText.compoundDrawablesRelative[0]
+            var clearDrawableShowed: Boolean
+
+            if (text.isNullOrEmpty()) {
+                clearDrawableShowed = false
+                setCompoundDrawablesRelative(null, null, searchDrawble, null)
+            } else {
+                clearDrawableShowed = true
+                setCompoundDrawablesRelative(null, null, clearDrawable, null)
+            }
+
+            addTextChangedListener { s: Editable? ->
+                if (s!=null){
+                    when{
+                        s.isEmpty() -> {
+                            setCompoundDrawablesRelative(null, null, searchDrawble, null)
+                            clearDrawableShowed = false
+                        }
+                        else -> if(!clearDrawableShowed) {
+                            setCompoundDrawablesRelative(null, null, clearDrawable, null)
+                            clearDrawableShowed = true
+                        }
+                    }
+                }
+            }
+
+            // clear버튼 동작
+            setOnTouchListener { v, event ->
+                var hasConsumed = false
+                if (v is EditText) {
+                    if (event.x >= v.width - v.totalPaddingRight) {
+                        if (event.action == MotionEvent.ACTION_UP && clearDrawableShowed) {
+                            text?.clear()
+                            setCompoundDrawablesRelative(null, null, searchDrawble, null)
+                            clearDrawableShowed = false
+                        }
+                        hasConsumed = true
+                    }
+                }
+                hasConsumed
+            }
+
+            // 키보드  엔터시에 생기는 동작
+            setOnKeyListener { v, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    val imm: InputMethodManager =
+                        activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(windowToken, 0)
+                    //todo search process
+                    searchViewModel.searchVideos(text.toString())
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+        }
+
 
 
         detailDialogButton.setOnClickListener {
@@ -75,7 +143,7 @@ class SearchFragment : Fragment() {
 
     private fun initViewModels() {
         with(searchViewModel) {
-            
+
         }
 
         with(mainSharedViewModel) {
